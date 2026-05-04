41547a212acc98f3150b9c47e0f45b8837ec2b31
Rohit Band <bandrohit906@gmail.com>
2026-05-04 10:52:20 +0530

    fix railway port

--- app.py
+++ app.py
@@ -1699,4 +1699,5 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
